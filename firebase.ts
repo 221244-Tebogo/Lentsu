e5d21ed02fdf968cd894808159adf919f2f432a1
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2025-08-17 12:10:08 +0200

    firebase removed sensetive info
    removed firebase

--- firebase.ts
+++ firebase.ts
@@ -8,14 +8,6 @@ import { getDatabase, ref, set } from "firebase/database"; //Realtime DB
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBEZIMhusON6__17T1uo5GDgZh0n_OJFq4",
-  authDomain: "lentsu-16a96.firebaseapp.com",
-  projectId: "lentsu-16a96",
-  storageBucket: "lentsu-16a96.firebasestorage.app",
-  messagingSenderId: "1044569860680",
-  appId: "1:1044569860680:web:e9c6109b1b0c3738e9c67d",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
